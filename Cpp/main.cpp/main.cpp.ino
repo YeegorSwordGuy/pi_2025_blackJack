e32e14f33223f40dd57fa341c8db84514f308578
Igor <igor.bernardi000@gmail.com>
2025-06-02 21:59:35 -0300

    Codigo LDC funcionando

--- Cpp/main.cpp/main.cpp.ino
+++ Cpp/main.cpp/main.cpp.ino
@@ -1,54 +1,41 @@
-// LiquidCrystal(RS, E, D4, D5, D6, D7)
-
+//Programa: Display LCD 16x2
+//Autor: MakerHero
+ 
+//Carrega a biblioteca LiquidCrystal
 #include <LiquidCrystal.h>
-
-
-#define D4 4
-#define D5 5
-#define D6 6
-#define D7 7
-#define E 8
-#define RS 9
-
-#define BtCompra 13
-#define BtPassa 12
-
-
-LiquidCrystal LCD(RS, E, D4, D5, D6, D7);  
-
-unsigned long ultimaLeitura = 0;
-const int atrasoDebounce = 200;
-
-void setup() { 
-	LCD.begin(16,2); 
-  
-   	pinMode(BtCompra, INPUT_PULLUP);
-   	pinMode(BtPassa, INPUT_PULLUP);
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup()
+{
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
 }
  
-void loop() {
-  
-  	LCD.clear();
-  	debugBotao();
-}
-
-
-void debugBotao() {
-  if (digitalRead(BtCompra) == LOW) {
-    if (millis() - ultimaLeitura > atrasoDebounce) {
-      ultimaLeitura = millis();
-      LCD.print("Compra");
-    }
+void loop()
+{
+  //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("VAMOO");
+  lcd.setCursor(3, 1);
+  lcd.print("GREMIO");
+  delay(800);
+   
+  //Rolagem para a esquerda
+  for (int posicao = 0; posicao < 3; posicao++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(300);
   }
-  if (digitalRead(BtPassa) == LOW) {
-    if (millis() - ultimaLeitura > atrasoDebounce) {
-      ultimaLeitura = millis();
-      LCD.print("Passa");
-    }
+   
+  //Rolagem para a direita
+  for (int posicao = 0; posicao < 6; posicao++)
+  {
+    lcd.scrollDisplayRight();
+    delay(300);
   }
 }
-
-// AI dealer
-
-// lógica de pontuação
-// -- armazenar as cartas 
