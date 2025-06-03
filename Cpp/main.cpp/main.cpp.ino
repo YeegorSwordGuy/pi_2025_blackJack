76ef79cc20389983b61b4f23c5d1c9578329ec31
Igor <igor.bernardi000@gmail.com>
2025-06-02 21:07:53 -0300

    Ideia inicial do codigo

--- Cpp/main.cpp/main.cpp.ino
+++ Cpp/main.cpp/main.cpp.ino
@@ -1,10 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+// LiquidCrystal(RS, E, D4, D5, D6, D7)
 
+#include <LiquidCrystal.h>
+
+
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define E 8
+#define RS 9
+
+#define BtCompra 13
+#define BtPassa 12
+
+
+LiquidCrystal LCD(RS, E, D4, D5, D6, D7);  
+
+unsigned long ultimaLeitura = 0;
+const int atrasoDebounce = 200;
+
+void setup() { 
+	LCD.begin(16,2); 
+  
+   	pinMode(BtCompra, INPUT_PULLUP);
+   	pinMode(BtPassa, INPUT_PULLUP);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  	LCD.clear();
+  	debugBotao();
 }
 
+
+void debugBotao() {
+  if (digitalRead(BtCompra) == LOW) {
+    if (millis() - ultimaLeitura > atrasoDebounce) {
+      ultimaLeitura = millis();
+      LCD.print("Compra");
+    }
+  }
+  if (digitalRead(BtPassa) == LOW) {
+    if (millis() - ultimaLeitura > atrasoDebounce) {
+      ultimaLeitura = millis();
+      LCD.print("Passa");
+    }
+  }
+}
+
+// AI dealer
+
+// lógica de pontuação
+// -- armazenar as cartas 
